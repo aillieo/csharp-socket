75a1dfea29a80337cf657f9e03e52bacb3e78676
aillieo <aillieo@163.com>
2017-04-17 01:27:06 +0800

    update client

--- csharp-client/Message.cs
+++ csharp-client/Message.cs
@@ -7,27 +7,43 @@ namespace csharp_client
 {
     class Message
     {
+        public Message(){ }
+
         private int _type;
-        public int type
-        {set{_type = type;} get{return _type;}}
+        public int Type
+        {
+            set{_type = value;}
+            get{return _type;}
+        }
 
         private int _length;
 
         private string _content;
-        public string content
-        { set { _content = content; } get { return _content; } }
-
-        int serializeToBytes(out byte[] data)
+        public string Content
         {
-            data = new byte[1];
-
-
-            return 0;
+            set{_content = value;}
+            get{return _content;}
         }
 
-        void ParseFromBytes(byte[] data)
+        public int serializeToBytes(out byte[] data)
         {
- 
+            _length = 4 + (int)_content.Length;
+            data = new byte[_length+1];
+            byte[] bType = BitConverter.GetBytes(_type);
+            Array.Copy(bType, 0, data, 0, 4);
+            byte[] bContent = System.Text.Encoding.Default.GetBytes(_content);
+            Array.Copy(bContent, 0, data, 4, _length - 4);
+            data[_length] = (byte)('\0');
+            return _length;
+        }
+
+        public void ParseFromBytes(byte[] data , int length)
+        {
+            if (data == null || data.Length == 0) { return; }
+            if (length < 4) { return; }
+            _length = length - 4;
+            _type = BitConverter.ToInt32(data,0);
+            _content = System.Text.Encoding.Default.GetString(data, 4, _length);   
         }
     }
 }
--- csharp-client/Program.cs
+++ csharp-client/Program.cs
@@ -10,8 +10,63 @@ namespace csharp_client
         static void Main(string[] args)
         {
             System.Console.WriteLine("hello client");
+
+            IPAddress ip = IPAddress.Parse(csharp_client.Config.server);
+            int port = csharp_client.Config.port;
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                clientSocket.Connect(new IPEndPoint(ip, port));
+                Console.WriteLine("connected");
+            }
+            catch
+            {
+                Console.WriteLine("connect fail");
+                return;
+            }
+
+
+
+
+
+            Message msg = new Message();
+            msg.Type = 0;
+            msg.Content = "hello server";
+
+
+            byte[] bMsg;
+            int len = msg.serializeToBytes(out bMsg);
+            byte[] bLen = BitConverter.GetBytes(len);
+            byte[] bSend = new byte[bMsg.Length + 4];
+            Array.Copy(bLen,0,bSend,0,4);
+            Array.Copy(bMsg,0,bSend,4,len);
+            clientSocket.Send(bSend);
+
             System.Console.ReadLine();
 
-        }  
+        }
+    }
+
+    class CommunicationManager
+    {
+        private CommunicationManager() { }
+        public static readonly CommunicationManager instance = new CommunicationManager();
+
+
+    }
+
+    class SocketClient
+    {
+    }
+
+    class UIDisplayer
+    {
+        private UIDisplayer() { }
+        public static readonly UIDisplayer instance = new UIDisplayer();
+
+
+
+
+
     }
 }
